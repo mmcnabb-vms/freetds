18a0768e1e8d90e8b397e9cd6220c52aeadd324d
Matt McNabb <matthew.mcnabb@vmssoftware.com>
2025-11-10 16:30:04 +1300

    dblib: add unit test d_bcp_defaultdate for bulk insert to column with default value
    Signed-off-by: Matt McNabb <matthew.mcnabb@vmssoftware.com>

---
+++ src/dblib/unittests/bcp_defaultdate.sql
@@ -0,0 +1,19 @@
+if exists (select 1 from sysobjects where type = 'U' and name = 'bcp_defaultdate') drop table bcp_defaultdate
+go
+CREATE TABLE bcp_defaultdate (
+	  not_null_int			int NOT NULL
+	, not_null_datetime		datetime DEFAULT GETDATE() NOT NULL
+)
+
+INSERT bcp_defaultdate(not_null_int)
+VALUES ( 12 )
+go
+
+select not_null_int, not_null_datetime from bcp_defaultdate
+go
+
+select not_null_int, not_null_datetime from bcp_defaultdate
+go
+
+drop table bcp_defaultdate
+go
